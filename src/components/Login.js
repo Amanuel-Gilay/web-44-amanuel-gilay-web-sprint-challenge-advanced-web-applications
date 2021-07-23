c45de409b7ad955b215d7cddc52c19bfb90c58b2
Amanuel Gilay <amanuelgilay@Amanuels-MacBook-Pro.local>
2021-07-23 15:01:25 -0700

    unfinished sprint

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,46 @@
 import React from "react";
+import axios from 'axios';
+import { render } from "@testing-library/react";
 
-const Login = () => {
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+
+  Login = (e) => {
+       e.preventDefault();
+       axios.post(`http://localhost:5000/api/login`, this.state.credentials)
+            .then(res => {
+              localStorage.setItem('token', res.data.token)
+              localStorage.setItem('username', res.data.username)
+              this.props.history.push('/protected');
+
+            })
+
+            .catch(err=>{
+              console.log(err);
+            })
+  
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
   const error = "";
   //replace with error state
-
+ render() {}
+ 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
